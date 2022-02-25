9782179289154e1cc1f99d0accb98086bc3578d6
Lucas Landin <88016189+lucas-landin@users.noreply.github.com>
2022-02-24 23:06:45 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -34,14 +34,14 @@ function showName(){
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName.name
+var varShowName = showName
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log("A função " + varShowName + " retorna " + showName() + ".")
+console.log("A função " + varShowName.name + " retorna " + showName() + ".")
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -109,6 +109,7 @@ var subtracao = calculadora("-");
 var multiplicacao = calculadora("*");
 var divisao = calculadora("/");
 var modulo = calculadora("%");
+
 
 
 /*
