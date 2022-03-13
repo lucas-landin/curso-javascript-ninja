d954e60adb7c15098b691085e0c4cf99c975071f
Lucas Landin <88016189+lucas-landin@users.noreply.github.com>
2022-03-13 17:38:42 -0300

    Update challenge-15.js

--- challenge-15/challenge-15.js
+++ challenge-15/challenge-15.js
@@ -1,3 +1,6 @@
+(function(){
+
+
 /*
 Envolva todo o código desse desafio em uma IIFE.
 Crie um arquivo chamado index.html e adicione esse script ao HTML.
@@ -21,7 +24,22 @@ as seguintes características:
   adicionados à idade original (age). Esse método deverá retornar o objeto
   que será instanciado.
 */
-// ?
+function Person(name, lastName, age){
+ this.name = name
+ this.lastName = lastName
+ this.age = age 
+ this.getFullName = function getFullName(){
+   return this.name + " " + this.lastName
+ };
+this.getAge = function getAge(){
+  return this.age
+};
+this.addAge = function addAge(){
+  this.age += arguments[0]
+  return this
+};
+
+};
 
 /*
 Crie 3 novos objetos usando o construtor acima. Os objetos serão novas
@@ -30,25 +48,42 @@ parâmetros corretamente para o construtor para criar as novas pessoas.
 Mostre as 3 novas pessoas criadas no console (Um console.log por pessoa).
 */
 console.log( 'Novas pessoas criadas à partir de Person:' );
-// ?
+var lucas = new Person('Lucas','Landin', 24 );
+console.log(lucas);
 
+var matheus = new Person('Matheus', 'Neder', 21);
+console.log(matheus)
+
+var calvin = new Person('Calvin', 'Chaves', 25);
+console.log(calvin)
 /*
 Mostre no console o nome completo de cada pessoa.
 */
 console.log( '\nNomes das pessoas:' );
-// ?
+console.log(lucas.getFullName());
+console.log(matheus.getFullName());
+console.log(calvin.getFullName());
 
 /*
 Mostre no console as idades de cada pessoa, com a frase:
 - "[NOME COMPLETO] tem [IDADE] anos."
 */
 console.log( '\nIdade das pessoas:' );
-// ?
+console.log(lucas.getAge());
+console.log(matheus.getAge());
+console.log(calvin.getAge());
 
 /*
 Adicione alguns anos à cada pessoa, e mostre no console a nova idade de
 cada um. A frase deverá ser no formato:
 - "[NOME COMPLETO] agora tem [NOVA IDADE] anos."
 */
+// Duas formas de fazer como o metodo addAge retorna o proprio objeto é possivel encadear metodos com o ponto 
 console.log( '\nNova idade das pessoas:' );
-// ?
+lucas.addAge(1);
+console.log(lucas.getFullName()+ ' agora tem ' + lucas.getAge() + ' anos ')
+
+console.log(matheus.getFullName()+ ' agora tem ' + matheus.addAge(1).getAge() + ' anos ')
+
+console.log(calvin.getFullName()+ ' agora tem ' + calvin.addAge(2).getAge() + ' anos ')
+})();
