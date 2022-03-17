2075d5b8152a3cb683d9d6b416c0198da1c303ef
Lucas Landin <88016189+lucas-landin@users.noreply.github.com>
2022-03-16 23:28:20 -0300

    Update challenge-18.js

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -1,10 +1,13 @@
+(function(){
+'use strict';
+
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
 
-/*
+/* cpfColection[0].replace(cpf,regexCpf)
 Crie uma função chamada `cleanCPF`, que receba um CPF por parâmetro, e
 retorne esse CPF limpo (somente os números).
 Usando os CPFs abaixo, mostre no console que a limpeza funciona para todos
@@ -15,7 +18,33 @@ eles! Use um console.log para cada CPF.
 - "101.123-131x32"
 */
 console.log( 'Limpando CPFs:' );
-// ?
+// função a baixo mostra minha resolução usando um array de cpfs como referência
+
+ /*function getcpf(index){
+    var cpfColection = ["049-214 3421-1","210.458.522-05","735 500 794 - 22","101.123-131x32"]
+    return cpfColection[index]
+ };
+
+ var regexCpf = /\D/g;
+
+ function cleanCPF(cpfIndex){
+    return getcpf(cpfIndex).match(regexCpf)
+}
+console.log(cleanCPF(0));
+*/
+//resolução mais direta com cpf em formato de string por parâmetro 
+var cpfColection = ["049-214 3421-1","210.458.522-05","735 500 794 - 22","101.123-131x32"]
+var regexCpf = /\D/g;
+
+function cleanCPF(cpf){
+    return cpf.replace(regexCpf, '')
+};
+console.log(cleanCPF("049-214 3421-1"));
+
+cpfColection.forEach(function(cpf){
+return console.log(cleanCPF(cpf))
+})
+    
 
 /*
 Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
@@ -23,8 +52,10 @@ Ex.: "999.999.999-99"
 Mostre o resultado no console.
 */
 console.log( '\nFormatando CPFs corretamente:' );
-// ?
-
+var regFormat = /(\d{3})(\d{3})(\d{3})(\d{2})/;
+cpfColection.forEach(function(cpf){
+    return console.log(cleanCPF(cpf).replace(regFormat, '$1.$2.$3-$4'))
+})
 /*
 Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
 usando o mínimo de caracteres possíveis na regex.
@@ -37,7 +68,7 @@ O resultado deve ser:
 ["junho", "julho"]
 */
 console.log( '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":' );
-// ?
+console.log("Os meses de janeiro, junho e julho começam com a letra j.".match(/ju[nl]ho/g))
 
 /*
 Crie uma expressão regular que faça o match com a abertura de uma tag
@@ -49,7 +80,7 @@ O resultado deve ser:
 ["<div>", "<section>", "<blockquote>"]
 */
 console.log( '\nMatch com a abertura de uma tag HTML:' );
-// ?
+console.log("<div><section><blockquote>Texto <img /></blockquote></section></div>".match(/<\w+>/g))
 
 /*
 Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
@@ -61,7 +92,7 @@ O resultado deve ser:
 ["<li></li>", "<li></li>", "<span></span>"]
 */
 console.log( '\nMatch com tags HTML vazias (abertura e fechamento da tag):' );
-// ?
+console.log("<div><ul><li></li><li></li><li><span></span></li></ul></div>".match(/<\w+><\/\w+>/g))
 
 /*
 Vamos complicar um pouco agora :D
@@ -86,4 +117,8 @@ https://regex101.com/#javascript e verifique se as capturas estão
 corretas, para depois aplicar no código ;)
 */
 console.log( '\nFazer replace dos textos das tags:' );
-// ?
+var text = "<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>"
+var regcont = /(<\w+>)([^<]+)(<\/\w+>)/g
+console.log(text.replace(regcont,'$1 O texto dentro da tag "$1" é "$2" $3 \n'))
+
+})();
